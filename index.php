f9da256b7489cf5ab92416857b6e224bc0e78c30
Guilherme Santos Carvalho <guicarvalho274@gmail.com>
2024-06-29 00:10:07 -0300

    Atualização no conteúdo em html e css + responsividade da pagina

--- index.php
+++ index.php
@@ -135,5 +135,189 @@
             </div><!--bg-->
         </div><!--container-->
     </section><!--sessao--two-->
+
+    <section class="sessao-three">
+        <div class="container">
+            <div class="flex">
+                <div class="col-one">
+                    <div class="subtitle">
+                        <p>Atenção</p>
+                    </div><!--subtitle-->
+                    <h2>Oportunidade de Emprego para <span>Crescimento e Ganhos Elevados!</span></h2>
+                    <p>Você está pronto para transformar sua carreira e alcançar o sucesso financeiro? Esta é a oportunidade que você esperava!
+                    <br><br>
+                    <span>Seja você um iniciante no mercado de trabalho ou alguém que já possui experiência,</span> mas busca maiores oportunidades de crescimento, nossa empresa está em busca de jovens talentos determinados a ganhar muito dinheiro e se destacarem, alcançando cargos elevados dentro da organização.</p>
+                </div><!--col-one-->
+
+                <div class="col-two">
+                    <div class="img one">
+                        <img src="<?php echo INCLUDE_PATH;?>imagens/img-box-one.png" alt="">
+                    </div><!--img-one-->
+
+                    <div class="img two">
+                    <img src="<?php echo INCLUDE_PATH;?>imagens/img-box-three.png" alt="">
+                    </div><!--img-two-->
+
+                    <div class="img three">
+                    <img src="<?php echo INCLUDE_PATH;?>imagens/img-box-two.png" alt="">
+                        
+                    </div><!--img-three-->
+                </div><!--col-two-->
+            </div><!--flex-->
+        </div><!--container-->
+    </section><!--three-->
+
+    <section class="sessao-four">
+        <div class="container">
+            <div class="flex">
+                <div class="col-one">
+                    <div class="box h100">
+                        <img src="<?php echo INCLUDE_PATH;?>imagens/img-box-h100.png" alt="">
+                    </div><!--h100-->
+
+                    <div class="box">
+                        <div class="h50">
+                            <img src="<?php echo INCLUDE_PATH;?>imagens/img-box-h50-one.png" alt="">
+                        </div><!--h50-->
+                        <div class="h50">
+                            <img src="<?php echo INCLUDE_PATH;?>imagens/img-box-h50-two.png" alt="">
+                        </div><!--h50-->
+                    </div><!--h50-->
+                </div><!--col-one-->
+
+                <div class="col-two">
+                    <p>Não importa onde você já trabalhou ou se este é seu primeiro emprego. O que importa é a sua vontade de crescer e fazer a diferença. <span>Nossa empresa, com 6 filiais em 3 estados e mais de 160 funcionários, oferece o ambiente perfeito para você iniciar sua jornada de sucesso.</span></p>
+                    <h3>Junte-se a nós e construa um 
+                    <span>futuro brilhante!</span></h3>
+
+                    <div class="btn-action" data-direct="form">
+                        <p>Quero a vaga</p>
+                    </div><!--btn-action-->
+                </div><!--col-two-->
+            </div><!--flex-->
+        </div><!--container-->
+    </section><!--four-->
+
+    <section class="sessao-five">
+        <div class="container">
+            <div class="subtitle">
+                <p>FAQ</p>
+            </div><!--subtitle-->
+            <h2><span>Perguntas Frequentes</span> sobre a Vaga:</h2>
+
+            <div class="label">
+                <div class="content-primary flex">
+                    <h4>Qual será meu horário de trabalho?</h4>
+                    <div class="icon">
+                        <img src="<?php echo INCLUDE_PATH;?>imagens/row.svg" alt="">
+                    </div><!--icon-->
+                </div><!--content-primary-->
+            </div><!--label-->
+
+            <div class="label">
+                <div class="content-primary flex">
+                    <h4>Quais são os dias de trabalho?</h4>
+                    <div class="icon">
+                        <img src="<?php echo INCLUDE_PATH;?>imagens/row.svg" alt="">
+                    </div><!--icon-->
+                </div><!--content-primary-->
+            </div><!--label-->
+
+            <div class="label">
+                <div class="content-primary flex">
+                    <h4>Quanto vou ganhar?</h4>
+                    <div class="icon">
+                        <img src="<?php echo INCLUDE_PATH;?>imagens/row.svg" alt="">
+                    </div><!--icon-->
+                </div><!--content-primary-->
+            </div><!--label-->
+
+            <div class="label">
+                <div class="content-primary flex">
+                    <h4>Qual é o salário fixo?</h4>
+                    <div class="icon">
+                        <img src="<?php echo INCLUDE_PATH;?>imagens/row.svg" alt="">
+                    </div><!--icon-->
+                </div><!--content-primary-->
+            </div><!--label-->
+
+            <div class="label">
+                <div class="content-primary flex">
+                    <h4>A carteira será assinada?</h4>
+                    <div class="icon">
+                        <img src="<?php echo INCLUDE_PATH;?>imagens/row.svg" alt="">
+                    </div><!--icon-->
+                </div><!--content-primary-->
+            </div><!--label-->
+
+            <div class="label">
+                <div class="content-primary flex">
+                    <h4>Quais são os benefícios?</h4>
+                    <div class="icon">
+                        <img src="<?php echo INCLUDE_PATH;?>imagens/row.svg" alt="">
+                    </div><!--icon-->
+                </div><!--content-primary-->
+            </div><!--label-->
+
+            <div class="label">
+                <div class="content-primary flex">
+                    <h4>Quais são os benefícios?</h4>
+                    <div class="icon">
+                        <img src="<?php echo INCLUDE_PATH;?>imagens/row.svg" alt="">
+                    </div><!--icon-->
+                </div><!--content-primary-->
+            </div><!--label-->
+
+            <h6>Lembrando: Se você tem filho(a) abaixo de 12 anos, a lei garante um acréscimo de
+            bônus por filho(a).</h6>
+
+            <h2>A oportunidade que você <span>tanto esperava chegou!</span> Construa sua carreira
+            conosco e ganhe muito dinheiro. <span>Candidate-se à vaga agora mesmo.</span></h2>
+
+            <div class="btn-action" data-direct="form">
+                <p>Estou dentro</p>
+            </div><!--btn-action-->
+        </div><!--container-->
+    </section><!--sessao-five-->
+
+    <section class="sessao_seven">
+        <div class="container">
+        <h4>Preencha todos os dados</h4>
+                <h2>Formulário de cadastro</h2>
+                <form autocomplete="off">
+                    <div class="flex">
+                        <div class="input">
+                            <label for="nome">Nome completo:</label>
+                            <input type="text" name="nome" id="nome" placeholder="Seu nome completo">
+                        </div><!--input-->
+    
+                        <div class="input">
+                            <label for="whatsapp">WhatsApp:</label>
+                            <input type="text" name="whatsapp" id="whatsapp" placeholder="99 9 9999-9999" maxlength="16">
+                        </div><!--input-->
+    
+                        <div class="input">
+                            <label for="experiencia">Tem Experiência?</label>
+                            <select name="experiencia" id="experiencia">
+                                <option value="Escolha uma opção" hidden>Escolha uma opção</option>
+                                <option value="Sim">Sim</option>
+                                <option value="Não">Não</option>
+                            </select>
+                        </div><!--input-->
+    
+                        <div class="input">
+                            <label for="email">Email:</label>
+                            <input type="email" name="email" id="email" placeholder="email@domain.com" autocomplete="off">
+                        </div><!--input-->
+    
+                        <input type="hidden" name="token" id="token" value="<?php echo $clientId?>">
+                        <div class="submit">
+                            <div class="load" id="loading"></div>
+                            <input type="submit" name="action" id="btn-action-form" value="Enviar dados">
+                        </div><!--submit-->
+                    </div>
+                </form>
+        </div><!--container-->
+    </section><!--sessao_seven-->
 </body>
 </html>
